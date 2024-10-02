28c6af09af8f7e5a5354e2e7fd64454ca8f6605a
nordinzakaria <nordinzakaria@gmail.com>
2024-10-02 08:49:59 +0800

    Initial commit

---
+++ Position.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPWeek5_VirtualWorker
+{
+    internal class Position
+    {
+        public float X {  get; set; }
+        public float Y { get; set; }
+    }
+}
---
+++ WorkerAvatar.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPWeek5_VirtualWorker
+{
+    internal class WorkerAvatar
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public int Id { get; set; }
+
+        public Position CurrPosition { get; set; }
+
+        public WorkerAvatar(string name, string description, int id)
+        {  Name = name; Description = description; Id = id; }
+
+        public WorkerAvatar() { }
+    }
+}
